b3b544c5ba1fdd39b2ad2fb4a5d3c925e77eef69
3ndetz <jayrawrr3@gmail.com>
2025-01-13 14:07:44 +0700

    Minor mixin altoclef.acceswidener fix maptextureaccessor

--- src/main/java/adris/altoclef/mixins/MapTextureAccessor.java
+++ src/main/java/adris/altoclef/mixins/MapTextureAccessor.java
@@ -1,11 +1,11 @@
 package adris.altoclef.mixins;
 
-import net.minecraft.client.render.MapRenderer;
+import net.minecraft.client.render.MapRenderer.MapTexture;
 import net.minecraft.client.texture.NativeImageBackedTexture;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(MapRenderer.MapTexture.class)
+@Mixin(MapTexture.class)
 public interface MapTextureAccessor {
 
     @Accessor("texture")
